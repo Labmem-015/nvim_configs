31e14906066a9a119929ebdf1462aba8cc3a2574
Labmem-015 <naumov.vlad04@gmail.com>
2026-06-29 18:21:31 +0300

    Add vim-plug and plugins

--- init.lua
+++ init.lua
@@ -3,49 +3,25 @@ vim.opt.shiftwidth = 4
 vim.opt.expandtab = false
 
 vim.opt.number = true
+vim.opt.relativenumber = true
 
+------- Key maps setup ------- 
 local map = vim.keymap.set
+local opts = { noremap = true, silent = true }
+-- noremap: prohibit recursive execution
+-- silent: hide command display at bottom
 
-map({'n', 'v', 'i'}, 'f', 'e')
-map({'n', 'v'}, 'p', 'r')
-map({'n', 'v'}, 'g', 't')
-map({'n', 'v'}, 'j', 'y')
-map({'n', 'v'}, 'l', 'u')
-map({'n', 'v'}, 'u', 'i')
-map({'n', 'v'}, 'y', 'o')
-map({'n', 'v'}, ':', 'p')
-map({'n', 'v'}, 'r', 's')
-map({'n', 'v'}, 's', 'd')
-map({'n', 'v'}, 't', 'f')
-map({'n', 'v'}, 'd', 'g')
-map({'n', 'v'}, 'n', 'j')
-map({'n', 'v'}, 'e', 'k')
-map({'n', 'v'}, 'i', 'l')
-map({'n', 'v'}, 'o', ':')
-map({'n', 'v'}, 'k', 'n')
+-- <mode>, <keys>, <action or keys>
+map('n', '<leader>n', ':NERDTreeFocus<CR>', opts)
+map('n', '<C-n>', ':NERDTree<CR>', opts)
+map('n', '<C-t>', ':NERDTreeToggle<CR>', opts)
+map('n', '<C-f>', ':NERDTreeFind<CR>', opts)
 
-map({'n', 'v'}, 'F', 'E')
-map({'n', 'v'}, 'P', 'R')
-map({'n', 'v'}, 'G', 'T')
-map({'n', 'v'}, 'J', 'Y')
-map({'n', 'v'}, 'L', 'U')
-map({'n', 'v'}, 'U', 'I')
-map({'n', 'v'}, 'Y', 'O')
-map({'n', 'v'}, ':', 'P')
-map({'n', 'v'}, 'R', 'S')
-map({'n', 'v'}, 'S', 'D')
-map({'n', 'v'}, 'T', 'F')
-map({'n', 'v'}, 'D', 'G')
-map({'n', 'v'}, 'N', 'J')
-map({'n', 'v'}, 'E', 'K')
-map({'n', 'v'}, 'I', 'L')
-map({'n', 'v'}, 'O', ':')
-map({'n', 'v'}, 'K', 'N')
+------- Plugins setup ------- 
+local Plug = vim.fn['plug#']
 
-
-
-
-
-
-
-
+vim.call('plug#begin')
+Plug('vim-airline/vim-airline')
+Plug('preservim/nerdtree')
+Plug('ryanoasis/vim-devicons') 
+vim.call('plug#end')
